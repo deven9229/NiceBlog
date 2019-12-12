479c6187757ee013ec66c12263afdb04c4da14b9
Deven Gurung <deven9229@gmail.com>
2019-12-12 14:04:49 +0600

    bug fix

--- app/policies/post_policy.rb
+++
@@ -1,9 +0,0 @@
-class PostPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      if user.admin?
-        scope.all
-      else
-        scope.where(published: true)
-      end
-end
