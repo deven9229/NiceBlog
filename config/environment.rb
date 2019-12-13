656be997ac2398a2448d4accce34e6140a32e3cf
Deven Gurung <deven9229@gmail.com>
2019-12-13 13:10:28 +0600

    configure ActionMailer to use Sendgrid in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'deven9229',
+  :password => 'Deven@123',
+  :domain => 'https://blooming-lake-90668.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
