80afbad25d990fa4b86db0725cf25fe46eb54dd6
Suman Gurung <sumangurung@gmail.com>
2019-12-14 10:51:36 -0500

    Persist user_id on post create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
    def create
     # render plain: params[:post].inspect
-    @post = Post.new(post_params)
+     @post = Post.new(post_params.merge(user_id: current_user.id))
     if(@post.save)
     redirect_to @post
   else
