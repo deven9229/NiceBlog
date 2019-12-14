e60cf24792c22134f8cd29fb6bfcb007cb0b18d9
Deven Gurung <deven9229@gmail.com>
2019-12-14 10:30:48 +0600

    made changes in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
+  config.action_mailer.default_url_options = { host: 'https://blooming-lake-90668.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+
 end
