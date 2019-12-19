71fd1faf61f86154063b4e68d80a22f0f910abf5
Deven Gurung <deven9229@gmail.com>
2019-12-19 11:35:48 +0600

    removed sendgrid from environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'app155544672@heroku.com',
-  :password => 'oo2iptb73181',
-  :domain => 'https://blooming-lake-90668.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
