8d4dc04181ea2c478cc5b7943e7439fcaf521057
Deven Gurung <deven9229@gmail.com>
2020-04-06 12:21:07 +0600

    changed the name of the project to diskos from navbar, footer and index page

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "should not save post without title" do
   post = Post.new
   assert_not post.save
