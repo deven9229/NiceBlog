7188a167640d5662ca925d32c94d6672919471da
Deven Gurung <deven9229@gmail.com>
2020-04-07 15:01:56 +0600

    mailtrap config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if ENV['MAILTRAP_HOST'].present?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['MAILTRAP_USER_NAME'],
+    :password => ENV['MAILTRAP_PASSWORD'],
+    :address => ENV['MAILTRAP_HOST'],
+    :port => ENV['MAILTRAP_PORT'],
+    :authentication => :plain
+  }
+end
